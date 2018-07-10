a8a698deeee4c975ef96907cf9e9fb32e841c573
Mikelle <mikhwall@gmail.com>
2018-07-11 01:19:17 +0300

    #118 merged with current dev and refactored DaoBaseImpersonated

--- contracts/DaoBaseImpersonated.sol
+++ contracts/DaoBaseImpersonated.sol
@@ -12,44 +12,42 @@ import "zeppelin-solidity/contracts/ECRecovery.sol";
 */
 contract ImpersonationCaller is DaoClient {
 
-	bytes32 public ISSUE_TOKENS = keccak256(abi.encodePacked("issueTokens"));
+    bytes32 public ISSUE_TOKENS = keccak256(abi.encodePacked("issueTokens"));
+    bytes32 constant public ALLOW_ACTION_BY_SHAREHOLDER = keccak256(abi.encodePacked("allowActionByShareholder"));
+    bytes32 constant public ALLOW_ACTION_BY_VOTING = keccak256(abi.encodePacked("allowActionByVoting"));
+    bytes32 constant public ALLOW_ACTION_BY_ADDRESS = keccak256(abi.encodePacked("allowActionByAddress"));
+    bytes32 constant public ALLOW_ACTION_BY_ANY_MEMBER_OF_GROUP = keccak256(abi.encodePacked("allowActionByAnyMemberOfGroup"));
+    bytes32 constant public REMOVE_GROUP_MEMBER = keccak256(abi.encodePacked("removeGroupMember"));
+    bytes32 constant public ADD_GROUP_MEMBER = keccak256(abi.encodePacked("addGroupMember"));
+    bytes32 constant public UPGRADE_DAO_CONTRACT = keccak256(abi.encodePacked("upgradeDaoContract"));
 
-  bytes32 constant public ISSUE_TOKENS = keccak256("issueTokens");
-  bytes32 constant public ALLOW_ACTION_BY_SHAREHOLDER = keccak256("allowActionByShareholder");
-  bytes32 constant public ALLOW_ACTION_BY_VOTING = keccak256("allowActionByVoting");
-  bytes32 constant public ALLOW_ACTION_BY_ADDRESS = keccak256("allowActionByAddress");
-  bytes32 constant public ALLOW_ACTION_BY_ANY_MEMBER_OF_GROUP = keccak256("allowActionByAnyMemberOfGroup");
-  bytes32 constant public REMOVE_GROUP_MEMBER = keccak256("removeGroupMember");
-  bytes32 constant public ADD_GROUP_MEMBER = keccak256("addGroupMember");
-  bytes32 constant public UPGRADE_DAO_CONTRACT = keccak256("upgradeDaoContract");
+    constructor(IDaoBase _dao) public DaoClient(_dao) {
 
-	constructor(IDaoBase _dao) public DaoClient(_dao) {
+    }
 
-	}
+    /**
+     * @dev Call action on behalf of the client
+     * @param _hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
+     * @param _sig bytes signature, the signature is generated using web3.eth.sign()
+     */
+    function doActionOnBehalfOf(bytes32 _hash, bytes _sig,
+        bytes32 _action, string _methodSig, bytes32[] _params) internal {
 
-  /**
-   * @dev Call action on behalf of the client
-   * @param _hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
-   * @param _sig bytes signature, the signature is generated using web3.eth.sign()
-   */
-	function doActionOnBehalfOf(bytes32 _hash, bytes _sig,
-										 bytes32 _action, string _methodSig, bytes32[] _params) internal {
+        // 1 - get the address of the client
+        address client = ECRecovery.recover(_hash, _sig);
 
-		// 1 - get the address of the client
-		address client = ECRecovery.recover(_hash, _sig);
+        // 2 - should be allowed to call action by a client
+        require(dao.isCanDoAction(client, _action));
 
-		// 2 - should be allowed to call action by a client
-		require(dao.isCanDoAction(client, _action));
-
-		// 3 - call
-		if(!address(dao).call(
-			bytes4(keccak256(abi.encodePacked(_methodSig))),
-			uint256(32),						 // pointer to the length of the array
-			uint256(_params.length),		 // length of the array
-			_params)){
-			revert();
-		}
-	}
+        // 3 - call
+        if (!address(dao).call(
+            bytes4(keccak256(abi.encodePacked(_methodSig))),
+            uint256(32), // pointer to the length of the array
+            uint256(_params.length), // length of the array
+            _params)) {
+            revert();
+        }
+    }
 }
 
 // TODO: convert to library?
@@ -64,93 +62,92 @@ contract ImpersonationCaller is DaoClient {
  * Please see 'tests' folder for example.
 */
 contract DaoBaseImpersonated is ImpersonationCaller {
+    event DaoBaseImpersonated_AllowActionByShareholder(string _what, address _tokenAddress);
+    event DaoBaseImpersonated_AllowActionByVoting(string _what, address _tokenAddress);
+    event DaoBaseImpersonated_AllowActionByAddress(string _group, address _a);
+    event DaoBaseImpersonated_AllowActionByAnyMemberOfGroup(string _what, string _groupName);
+    event DaoBaseImpersonated_AddGroup(string _group);
+    event DaoBaseImpersonated_RemoveGroupMember(string _groupName, address _a);
+    event DaoBaseImpersonated_AddGroupMember(string _groupName, address _a);
+    constructor(IDaoBase _dao)public
+    ImpersonationCaller(_dao)
+    {
+    }
 
-  event DaoBaseImpersonated_AllowActionByShareholder(string _what, address _tokenAddress);
-  event DaoBaseImpersonated_AllowActionByVoting(string _what, address _tokenAddress);
-  event DaoBaseImpersonated_AllowActionByAddress(string _group, address _a);
-  event DaoBaseImpersonated_AllowActionByAnyMemberOfGroup(string _what, string _groupName);
-  event DaoBaseImpersonated_AddGroup(string _group);
-  event DaoBaseImpersonated_RemoveGroupMember(string _groupName, address _a);
-  event DaoBaseImpersonated_AddGroupMember(string _groupName, address _a);
-  constructor(IDaoBase _dao)public
-		ImpersonationCaller(_dao)
-	{
-	}
+    function upgradeDaoContractImp(bytes32 _hash, bytes _sig, address _newMc) public {
+        bytes32[] memory params = new bytes32[](1);
+        params[0] = bytes32(_newMc);
 
-  function upgradeDaoContractImp(bytes32 _hash, bytes _sig, address _newMc) public {
-    bytes32[] memory params = new bytes32[](1);
-    params[0] = bytes32(_newMc);
+        return doActionOnBehalfOf(_hash, _sig, UPGRADE_DAO_CONTRACT, "upgradeDaoContractGeneric(bytes32[])", params);
+    }
 
-    return doActionOnBehalfOf(_hash, _sig, UPGRADE_DAO_CONTRACT, "upgradeDaoContractGeneric(bytes32[])", params);
-  }
+    function issueTokensImp(bytes32 _hash, bytes _sig,
+        address _token, address _to, uint _amount) public {
+        bytes32[] memory params = new bytes32[](3);
+        params[0] = bytes32(_token);
+        params[1] = bytes32(_to);
+        params[2] = bytes32(_amount);
 
-	function issueTokensImp(bytes32 _hash, bytes _sig,
-									 address _token, address _to, uint _amount) public {
-		bytes32[] memory params = new bytes32[](3);
-		params[0] = bytes32(_token);
-		params[1] = bytes32(_to);
-		params[2] = bytes32(_amount);
+        return doActionOnBehalfOf(_hash, _sig, ISSUE_TOKENS, "issueTokensGeneric(bytes32[])", params);
+    }
 
-	   return doActionOnBehalfOf(_hash, _sig, ISSUE_TOKENS, "issueTokensGeneric(bytes32[])", params);
-	}
+    function allowActionByShareholderImp(bytes32 _hash, bytes _sig, string _what, address _tokenAddress) public {
+        emit DaoBaseImpersonated_AllowActionByShareholder(_what, _tokenAddress);
+        bytes32[] memory params = new bytes32[](2);
+        params[0] = bytes32(keccak256(_what));
+        params[1] = bytes32(_tokenAddress);
 
-  function allowActionByShareholderImp(bytes32 _hash, bytes _sig, string _what, address _tokenAddress) public {
-    emit DaoBaseImpersonated_AllowActionByShareholder(_what, _tokenAddress);
-    bytes32[] memory params = new bytes32[](2);
-    params[0] = bytes32(keccak256(_what));
-    params[1] = bytes32(_tokenAddress);
+        return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_SHAREHOLDER, "allowActionByShareholderGeneric(bytes32[])", params);
+    }
 
-    return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_SHAREHOLDER, "allowActionByShareholderGeneric(bytes32[])", params);
-  }
+    function allowActionByVotingImp(bytes32 _hash, bytes _sig, string _what, address _tokenAddress) public {
+        emit DaoBaseImpersonated_AllowActionByVoting(_what, _tokenAddress);
+        bytes32[] memory params = new bytes32[](2);
+        params[0] = bytes32(keccak256(_what));
+        params[1] = bytes32(_tokenAddress);
 
-  function allowActionByVotingImp(bytes32 _hash, bytes _sig, string _what, address _tokenAddress) public {
-    emit DaoBaseImpersonated_AllowActionByVoting(_what, _tokenAddress);
-    bytes32[] memory params = new bytes32[](2);
-    params[0] = bytes32(keccak256(_what));
-    params[1] = bytes32(_tokenAddress);
+        return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_VOTING, "allowActionByVotingGeneric(bytes32[])", params);
+    }
 
-    return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_VOTING, "allowActionByVotingGeneric(bytes32[])", params);
-  }
+    function allowActionByAddressImp(bytes32 _hash, bytes _sig, string _what, address _a) public {
+        emit DaoBaseImpersonated_AllowActionByAddress(_what, _a);
+        bytes32[] memory params = new bytes32[](2);
+        params[0] = bytes32(keccak256(_what));
+        params[1] = bytes32(_a);
 
-  function allowActionByAddressImp(bytes32 _hash, bytes _sig, string _what, address _a) public {
-    emit DaoBaseImpersonated_AllowActionByAddress(_what, _a);
-    bytes32[] memory params = new bytes32[](2);
-    params[0] = bytes32(keccak256(_what));
-    params[1] = bytes32(_a);
+        return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_ADDRESS, "allowActionByAddressGeneric(bytes32[])", params);
+    }
 
-    return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_ADDRESS, "allowActionByAddressGeneric(bytes32[])", params);
-  }
+    function allowActionByAnyMemberOfGroupImp(bytes32 _hash, bytes _sig, string _what, string _groupName) public {
+        emit DaoBaseImpersonated_AllowActionByAnyMemberOfGroup(_what, _groupName);
+        bytes32[] memory params = new bytes32[](2);
+        params[0] = bytes32(keccak256(_what));
+        params[1] = bytes32(keccak256(_groupName));
 
-  function allowActionByAnyMemberOfGroupImp(bytes32 _hash, bytes _sig, string _what, string _groupName) public {
-    emit DaoBaseImpersonated_AllowActionByAnyMemberOfGroup(_what, _groupName);
-    bytes32[] memory params = new bytes32[](2);
-    params[0] = bytes32(keccak256(_what));
-    params[1] = bytes32(keccak256(_groupName));
+        return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_ANY_MEMBER_OF_GROUP, "allowActionByAnyMemberOfGroupGeneric(bytes32[])", params);
+    }
 
-    return doActionOnBehalfOf(_hash, _sig, ALLOW_ACTION_BY_ANY_MEMBER_OF_GROUP, "allowActionByAnyMemberOfGroupGeneric(bytes32[])", params);
-  }
+    function removeGroupMemberImp(bytes32 _hash, bytes _sig, string _groupName, address _a) public {
+        emit DaoBaseImpersonated_RemoveGroupMember(_groupName, _a);
+        bytes32[] memory params = new bytes32[](2);
+        params[1] = bytes32(keccak256(_groupName));
+        params[1] = bytes32(_a);
 
-  function removeGroupMemberImp(bytes32 _hash, bytes _sig, string _groupName, address _a) public {
-    emit DaoBaseImpersonated_RemoveGroupMember(_groupName, _a);
-    bytes32[] memory params = new bytes32[](2);
-    params[1] = bytes32(keccak256(_groupName));
-    params[1] = bytes32(_a);
+        return doActionOnBehalfOf(_hash, _sig, REMOVE_GROUP_MEMBER, "removeGroupMemberGeneric(bytes32[])", params);
+    }
 
-    return doActionOnBehalfOf(_hash, _sig, REMOVE_GROUP_MEMBER, "removeGroupMemberGeneric(bytes32[])", params);
-  }
+    function addGroupMemberImp(bytes32 _hash, bytes _sig, string _groupName, address _a) public {
+        emit DaoBaseImpersonated_AddGroupMember(_groupName, _a);
+        bytes32[] memory params = new bytes32[](2);
+        params[1] = bytes32(keccak256(_groupName));
+        params[1] = bytes32(_a);
 
-  function addGroupMemberImp(bytes32 _hash, bytes _sig, string _groupName, address _a) public {
-    emit DaoBaseImpersonated_AddGroupMember(_groupName, _a);
-    bytes32[] memory params = new bytes32[](2);
-    params[1] = bytes32(keccak256(_groupName));
-    params[1] = bytes32(_a);
-
-    return doActionOnBehalfOf(_hash, _sig, ADD_GROUP_MEMBER, "addGroupMemberGeneric(bytes32[])", params);
-  }
+        return doActionOnBehalfOf(_hash, _sig, ADD_GROUP_MEMBER, "addGroupMemberGeneric(bytes32[])", params);
+    }
 
     // TODO: add other methods:
-	/*
-	function addGroup(string _groupName) public isCanDo("manageGroups")
+    /*
+    function addGroup(string _groupName) public isCanDo("manageGroups")
    ...
    */
 }
